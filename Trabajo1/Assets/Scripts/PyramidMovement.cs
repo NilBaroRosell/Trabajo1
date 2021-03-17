585faf6346400bee2975c88224c88a1866aa2414
Nil <nil.baro@enti.cat>
2021-03-17 18:58:23 +0100

    input, pickups and shit

--- Trabajo1/Assets/Scripts/PyramidMovement.cs
+++ Trabajo1/Assets/Scripts/PyramidMovement.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PyramidMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private InputManager input;
+    private Rigidbody rb;
+    [SerializeField] private float velocity = 2.0f;
+    [SerializeField] private float jumpForce = 10.0f;
+    private bool isJumping = false;
+
+    private void Awake()
     {
-        
+        input = gameObject.GetComponent<InputManager>();
+        rb = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(input.GetLeft())
+        {
+            rb.AddForce(Vector3.right * velocity, ForceMode.Force);
+        }
+        if(input.GetRight())
+        {
+            rb.AddForce(-Vector3.right * velocity, ForceMode.Force);
+        }
+        if(input.GetJump())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Pyramid")
+        {
+            isJumping = false;
+        }
     }
 }
---
+++ Trabajo1/Assets/Scripts/PyramidPickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramidPickUp : MonoBehaviour
+{
+    public int score;
+    private int points = 15;
+
+    private void Awake()
+    {
+        score = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Eye")
+        {
+            AddScore();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void AddScore()
+    {
+        score += points;
+    }
+}
