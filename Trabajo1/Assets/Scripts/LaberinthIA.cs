6bc8a5140c8bcbbd38b2b9dd9b63789e6011adc4
Bartomeu <bartomeu.puig@enti.cat>
2021-03-20 18:59:03 +0100

    Laberinth done

---
+++ Trabajo1/Assets/Scripts/LaberinthIA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaberinthIA : MonoBehaviour
+{
+    Vector3[] points;
+    int idx = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = new Vector3[transform.childCount-1];
+
+        for(int i = 0; transform.childCount-1 > i; i++)
+        {
+            points[i] = transform.GetChild(i).position;
+            Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.Normalize(points[idx] - transform.position) / 35;
+        if (Vector3.Distance(transform.position, points[idx]) < 0.5f && idx < points.Length) idx++;
+    }
+}
---
+++ Trabajo1/Assets/Scripts/LaberinthMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaberinthMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(shine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized / speed;
+    }
+
+    IEnumerator shine()
+    {
+        transform.GetChild(0).transform.localPosition += Vector3.up * 20;
+        while (light.intensity < 1)
+        {
+            light.intensity += 0.01f;
+            yield return new WaitForFixedUpdate();
+        }
+
+        yield return new WaitForSeconds(3);
+
+        while (light.intensity > 0)
+        {
+            light.intensity -= 0.01f;
+            yield return new WaitForFixedUpdate();
+        }
+
+        transform.GetChild(0).transform.localPosition += Vector3.down * 20;
+
+        yield return new WaitForSeconds(10);
+
+        StartCoroutine(shine());
+        yield break;
+    }
+}
