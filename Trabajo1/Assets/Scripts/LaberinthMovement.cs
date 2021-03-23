acece129440fc5e4b8dd61b456aa514c04376729
Bartomeu <bartomeu.puig@enti.cat>
2021-03-23 23:45:45 +0100

    Laberinth controlled by manager

--- Trabajo1/Assets/Scripts/LaberinthMovement.cs
+++ Trabajo1/Assets/Scripts/LaberinthMovement.cs
@@ -6,17 +6,31 @@ public class LaberinthMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] Light light;
+    List<KeyValuePair<int, string>> ranking;
+    public int[] points;
+    bool finished = false;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        ranking = new List<KeyValuePair<int, string>>();
+        points = new int[4];
         StartCoroutine(shine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized / speed;
+        if (!finished)
+        {
+            transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized / speed;
+            hasFinished(transform);
+        }
+        else
+        {
+            if (ranking.Count == 4) showClasification();
+        }
     }
 
     IEnumerator shine()
@@ -42,5 +56,37 @@ public class LaberinthMovement : MonoBehaviour
 
         StartCoroutine(shine());
         yield break;
+    }
+
+    public void hasFinished(Transform _player)
+    {
+       if(_player.position.x > 92)
+        {
+            ranking.Add(new KeyValuePair<int, string>(10 - ranking.Count * 3, _player.name));
+            Debug.Log(ranking[ranking.Count - 1]);
+            if (_player != transform)
+            {
+                _player.GetComponent<LaberinthIA>().enabled = false;
+            }
+            else
+            {
+                finished = true;
+            }
+        }
+    }
+
+    void showClasification()
+    {
+        points[0] = ranking.Find(x => x.Value == "Red").Key;
+        points[1] = ranking.Find(x => x.Value == "Green").Key;
+        points[2] = ranking.Find(x => x.Value == "Blue").Key;
+        points[3] = ranking.Find(x => x.Value == "Pink").Key;
+
+        Invoke("EndOfTheGame", 10.0f);
+    }
+
+    void EndOfTheGame()
+    {
+        gameOver = true;
     }
 }
